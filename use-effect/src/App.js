78b61ba9d3f0c2d09a1650cef35f4210109e56fd
umesh@8681 <umamaheswaran.n@securekloud.com>
2022-09-17 18:57:58 +0530

    use-effect best practices - componentDidMount, componentDidUpdate covered

---
+++ use-effect/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Example1 from "./components/Example1";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign : 'center'}}>
+        <Example1/>
+      </div>
+    );
+  }
+}
+export default App;
